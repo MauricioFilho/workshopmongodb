8d9a9f21b0683430cd509f76c930cc3ac8d474c6
Mauricio Goulart <mauriciogoulart23@live.com>
2021-01-28 13:19:47 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/mauriciogoulart/workshopmongodb/domain/Post.java
+++ src/main/java/com/mauriciogoulart/workshopmongodb/domain/Post.java
@@ -1,12 +1,15 @@
 package com.mauriciogoulart.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mauriciogoulart.workshopmongodb.dto.AuthorDTO;
+import com.mauriciogoulart.workshopmongodb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,9 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 		
 		
@@ -74,6 +80,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	
 	@Override
 	public int hashCode() {
@@ -100,6 +114,7 @@ public class Post implements Serializable {
 		return true;
 	}
 
+	
 
 	
 }
