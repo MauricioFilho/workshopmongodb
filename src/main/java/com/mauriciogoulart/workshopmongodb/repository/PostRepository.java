bce4925739222681cdda6c91b5b4e786cb6a72bf
Mauricio Goulart <mauriciogoulart23@live.com>
2021-01-28 14:29:53 -0300

    Consulta simples com @Query

--- src/main/java/com/mauriciogoulart/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/mauriciogoulart/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.mauriciogoulart.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mauriciogoulart.workshopmongodb.domain.Post;
@@ -12,5 +13,8 @@ import com.mauriciogoulart.workshopmongodb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'Title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 }
